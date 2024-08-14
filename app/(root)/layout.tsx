a2d93a5b05551022636cd6e3253f34331c8f93f4
xtc2016xtc <728611827@qq.com>
2024-08-14 18:20:12 +0800

    新增移动端导航栏的基础创建

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+import Sidebar from "@/components/left/Sidebar";
+import {logoIcon} from "@/utils";
+import Image from "next/image";
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = {firstName: '熊先生',lastName:'bear'}
+
+
     return (
-        <main>
-            STDEBAR
+        <main className="flex h-screen w-full font-inter">
+            {/*@ts-ignore*/}
+            <Sidebar user={loggedIn} />
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image
+                        src={logoIcon}
+                        width={30}
+                        height={30}
+                        alt="menu icon"
+                    />
+                </div>
+            </div>
             {children}
         </main>
     );
 }
+
+
+
