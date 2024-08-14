62e84aec424889a0bd80ea0a08a6564c34181d87
xtc2016xtc <728611827@qq.com>
2024-08-14 10:46:08 +0800

    欢迎，头部自定义组件[ type?=类型 title=欢迎词 user=用户id用户名 subtext=基础介绍 ]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,30 @@
 import {Button} from "@/components/ui/button";
+import HeaderBox from "@/components/Header/HeaderBox";
 
 const Home = () => {
+    const loggedIn = {firstName:'xtc'}
+
+
+
     return (
-        <div>
-            <Button className={`text-amber-600`}>
-                Page
-            </Button>
-        </div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficently"
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
 export default Home
+
+/*
+* 33:07
+*
+* */
