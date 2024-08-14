9131b6509c5dc9fa651ca8538947419467bd8045
xtc2016xtc <728611827@qq.com>
2024-08-14 18:19:31 +0800

    新增SidebarIcon图像导出方式

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,38 @@
 /*
 * conins icon
 * */
-export const coninsIcon = '/icons/coins.svg'
+// export const coninsIcon = '/icons/coins.svg'
 
 /*
 * logo svg
 * */
 export const logoIcon = '/icons/logo.svg'
+
+/*
+* SidebarIcon
+* */
+//首页
+export const HomeIcon = '/icons/home.svg'
+
+//我的银行
+export const BanksIcon = '/icons/dollar-circle.svg'
+
+//交易历史
+export const transactionIcon = '/icons/transaction.svg'
+
+//转账
+export const moneyIcon = '/icons/money-send.svg'
+
+
+
+
+
+
+/*
+* 公共样式
+* */
+export const desiderata = {
+        nav:"flex flex-col gap-4",
+        Stylelink:"mb-12 cursor-pointer gap-2",
+        logostyle:"size-[24px] max-xl:size-14"
+    }
