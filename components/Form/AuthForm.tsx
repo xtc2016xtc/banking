3ea5381c9cb86a6dcae5a310ffccb507075bccaf
xtc2016xtc <728611827@qq.com>
2024-08-19 18:43:10 +0800

    新增注册表单

--- components/Form/AuthForm.tsx
+++ components/Form/AuthForm.tsx
@@ -1,72 +1,104 @@
-"use client"
-import Link from "next/link";
-import Image from "next/image";
-import {logoIcon} from "@/utils";
-import { useState } from "react";
+'use client';
 
-import { z } from "zod";
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
-})
+import { Form } from "@/components/ui/form"
+import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+// import { useRouter } from 'next/navigation';
+import CustomInput from "@/components/Header/CustomInput";
+import {logoIcon} from "@/utils";
 
 const AuthForm = ({ type }: { type: string }) => {
+    // const router = useRouter();
+    const [user, setUser] = useState(null);
+    setUser(user);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formSchema = authFormSchema(type);
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
-            password:""
+            password: ''
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        setIsLoading(true);
+        console.log(data)
+
+        try {
+            // Sign up with Appwrite & create plaid token
+
+            if(type === 'sign-up') {
+                // const userData = {
+                //     firstName: data.firstName!,
+                //     lastName: data.lastName!,
+                //     address1: data.address1!,
+                //     city: data.city!,
+                //     state: data.state!,
+                //     postalCode: data.postalCode!,
+                //     dateOfBirth: data.dateOfBirth!,
+                //     ssn: data.ssn!,
+                //     email: data.email,
+                //     password: data.password
+                // }
+                //
+                // const newUser = await signUp(userData);
+                //
+                // setUser(newUser);
+            }
+
+            if(type === 'sign-in') {
+                // const response = await signIn({
+                //     email: data.email,
+                //     password: data.password,
+                // })
+                //
+                // if(response) router.push('/')
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
-    const [user, setUser] = useState()
-
-    setUser(user)
     return (
         <section className="auth-form">
-            <header className="flex flex-col gap-5 md:gap-8">
-                <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+            <header className='flex flex-col gap-5 md:gap-8'>
+                <Link href="/" className="cursor-pointer flex items-center gap-1">
                     <Image
                         src={logoIcon}
                         width={34}
                         height={34}
                         alt="Horizon logo"
                     />
-                    <h1 className="text-26 font-ibm-plex-serif text-black-1 font-extrabold">Horizon</h1>
+                    <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
                 </Link>
+
                 <div className="flex flex-col gap-1 md:gap-3">
                     <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
                         {user
                             ? 'Link Account'
                             : type === 'sign-in'
-                            ? 'Sign In'
-                                : 'Sign up'
+                                ? '登录'
+                                : '注册'
                         }
-                        <p className="tetx-16 font-normal text-gray-600">
+                        <p className="text-16 font-normal text-gray-600">
                             {user
-                                ? 'Link Account'
-                                : 'Please enter your details'
+                                ? 'Link your account to get started'
+                                : '请输入您的详细信息以便更好的服务'
                             }
                         </p>
                     </h1>
@@ -74,49 +106,59 @@ const AuthForm = ({ type }: { type: string }) => {
             </header>
             {user ? (
                 <div className="flex flex-col gap-4">
-                    {/*PlaidLink*/}
+                    {/*<PlaidLink user={user} variant="primary" />*/}
                 </div>
-            ):(
+            ): (
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                   <div className="form-item">
-                                       <FormLabel className="form-label">
-                                           Email
-                                       </FormLabel>
-                                       <div className="flex w-full flex-col">
-                                            <FormControl>
-                                                <Input placeholder="请输入邮件" className="input-class" {...field} />
-                                            </FormControl>
-                                           <FormMessage className="form-message mt-2"/>
-                                       </div>
-                                   </div>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <div className="form-item">
-                                        <FormLabel className="form-label">
-                                            password
-                                        </FormLabel>
-                                        <div className="flex w-full flex-col">
-                                            <FormControl>
-                                                <Input placeholder="请输入密码" className="input-class" {...field} />
-                                            </FormControl>
-                                            <FormMessage className="form-message mt-2"/>
-                                        </div>
+                            {type === 'sign-up' && (
+                                <>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='firstName' label="姓氏" placeholder='确认您的姓氏' />
+                                        <CustomInput control={form.control} name='lastName' label="名字" placeholder='确认您的名字' />
                                     </div>
-                                )}
-                            />
-                            <Button type="submit">Submit</Button>
+                                    <CustomInput control={form.control} name='address1' label="地址" placeholder='确认您的地址' />
+                                    <CustomInput control={form.control} name='city' label="城市" placeholder='确认您的城市' />
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='state' label="所在国家" placeholder='例如：纽约' />
+                                        <CustomInput control={form.control} name='postalCode' label="邮政编码" placeholder='例如: 11101' />
+                                    </div>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='dateOfBirth' label="出生日期" placeholder='默认' />
+                                        <CustomInput control={form.control} name='ssn' label="SSN" placeholder='您的身份信息' />
+                                    </div>
+                                </>
+                            )}
+
+                            <CustomInput control={form.control} name='email' label="注册所用的邮件" placeholder='您的邮件' />
+
+                            <CustomInput control={form.control} name='password' label="密码" placeholder='确认您的密码' />
+
+                            <div className="flex flex-col gap-4">
+                                <Button type="submit" disabled={isLoading} className="form-btn">
+                                    {isLoading ? (
+                                        <>
+                                            <Loader2 size={20} className="animate-spin" /> &nbsp;
+                                            Loading...
+                                        </>
+                                    ) : type === 'sign-in'
+                                        ? '登录' : '注册'}
+                                </Button>
+                            </div>
                         </form>
                     </Form>
+
+                    <footer className="flex justify-center gap-1">
+                        <p className="text-14 font-normal text-gray-600">
+                            {type === 'sign-in'
+                                ? "没有账户?"
+                                : "已有账户?"}
+                        </p>
+                        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
+                            {type === 'sign-in' ? '前往注册' : '返回登录'}
+                        </Link>
+                    </footer>
                 </>
             )}
         </section>
