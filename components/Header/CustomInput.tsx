faf37966842bff2468d6396977d5adb53ea70360
xtc2016xtc <728611827@qq.com>
2024-08-20 11:58:47 +0800

    删除多余的内容

--- components/Header/CustomInput.tsx
+++ components/Header/CustomInput.tsx
@@ -1,36 +1,40 @@
 import React from 'react'
-
 import { Control, FieldPath } from 'react-hook-form'
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
-import {FormControl, FormField, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
+import { FormControl, FormField, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = authFormSchema('sign-up')
 
-interface CustomInput {
+interface CustomInputProps {
     control: Control<z.infer<typeof formSchema>>,
     name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder: string
 }
 
-const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+const CustomInput: React.FC<CustomInputProps> = ({ control, name, label, placeholder }) => {
+    const inputId = `form-item-${name}`; // Generate a unique id based on the name
+    const autocomplete = name === 'password' ? 'current-password' : 'off'
+
     return (
         <FormField
             control={control}
             name={name}
             render={({ field }) => (
                 <div className="form-item">
-                    <FormLabel className="form-label">
+                    <FormLabel className="form-label" htmlFor={inputId}>
                         {label}
                     </FormLabel>
                     <div className="flex w-full flex-col">
                         <FormControl>
                             <Input
+                                id={inputId} // Assign the generated id here
                                 placeholder={placeholder}
                                 className="input-class"
                                 type={name === 'password' ? 'password' : 'text'}
+                                autoComplete={autocomplete}
                                 {...field}
                             />
                         </FormControl>
@@ -43,3 +47,4 @@ const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
 }
 
 export default CustomInput
+
